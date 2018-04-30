65096112a251a0d7c5416d0ee19b4e43f4fdf515
laurentbp <l.blancpattin@gmail.com>
2018-04-30 23:30:27 +0200

    Javascript et update css (page d'accueil)

--- index.php
+++ index.php
@@ -53,25 +53,104 @@
 			</div>
 		</header>
 
+		<a class="top-anchor" href="#" ><span class="glyphicon glyphicon-arrow-up"></span></a>
+
 		<div class="main-content text-center">
 			<div class="rubrique about">
+				<h2>BONJOUR JE SUIS LE CAPITAINE PETIT COCHON MIGNON RRRON RRRON</h2>
 				<h2>Êtes-vous...</h2>
-				<div class="student-redirection col-lg-6 col-md-6 col-sm-6">
-					<h4>...Un étudiant ?</h4>
-					<span class="glyphicon glyphicon-education"></span>
-					<p>Vous cherchez à participer à une étude ? Vous voulez vous faire un peu d'argent tout en travaillant pour le compte de grandes entreprises ? Vous souhaitez intégrer la JEMA en tant que membre actif ?</p>
-					<p>Alors <a href="student.php">cliquez ici !</a></p>
-				</div>
-				<div class="company-redirection col-lg-6 col-md-6 col-sm-6">
-					<h4>...Une entreprise ?</h4>
-					<span class="glyphicon glyphicon-briefcase"></span>
-					<p>Vous cherchez à participer à une étude ? Vous voulez vous faire un peu d'argent tout en travaillant pour le compte de grandes entreprises ? Vous souhaitez intégrer la JEMA en tant que membre actif ?</p>
-					<p>Alors <a href="company.php">cliquez ici !</a></p>
-				</div>
+				<a href="student.php">
+					<div class="student-redirection col-lg-6 col-md-6 col-sm-6">
+						<div class="left-student">
+							<h4>...Un étudiant ?</h4>
+							<span class="big-glyphicon glyphicon glyphicon-education"></span>
+							<p>Vous cherchez à participer à une étude ? Vous voulez vous faire un peu d'argent tout en travaillant pour le compte de grandes entreprises ? Vous souhaitez intégrer la JEMA en tant que membre actif ?</p>
+						</div>
+						<div class="right-student">
+							<span class="glyphicon glyphicon-chevron-right"></span>
+						</div>
+					</div>
+				</a>
+				<a href="company.php">
+					<div class="company-redirection col-lg-6 col-md-6 col-sm-6">
+						<div class="left-company">
+							<h4>...Une entreprise ?</h4>
+							<span class="big-glyphicon glyphicon glyphicon-briefcase"></span>
+							<p>Vous cherchez une étude dans le domaine de l'agronomie ? Vous disposez d'un budget trop serré pour faire appel aux bureaux d'étude traditionnels ? Vous souhaitez avoir la garantie d'un résultat à la fois économique et professionnel ?</p>
+						</div>
+						<div class="right-company">
+							<span class="glyphicon glyphicon-chevron-right"></span>
+						</div>
+					</div>
+				</a>
 			</div>
 		</div>
 
-		<footer>
+		<footer class="text-center">
+			<div class="col-lg-3 col-md-3 col-sm-6">
+				<h3>Plan du site</h3>
+				<ul class="footer-list">
+					<li><h4><a href="index.php">Accueil</a></h4></li>
+					<li>
+						<h4><a href="student.php">Page étudiants</a></h4>
+						<ul class="inner-footer-list">
+							<li><a href="student.php#about">Qui sommes-nous ?</a></li>
+							<li><a href="student.php#numbers">Quelques chiffres</a></li>
+							<li><a href="student.php#faq">Participer à une étude</a></li>
+							<li><a href="student.php#examples">Examples d'études</a></li>
+							<li><a href="student.php#member">Devenir membre actif</a></li>
+							<li><a href="student.php#news">News</a></li>
+						</ul>
+					</li>
+					<li>
+						<h4><a href="company.php">Page entreprise</a></h4>
+						<ul class="inner-footer-list">
+							<li><a href="company.php#about">Qui sommes-nous ?</a></li>
+							<li><a href="company.php#numbers">Quelques chiffres</a></li>
+							<li><a href="company.php#skills">Nos domaines de compétences</a></li>
+							<li><a href="company.php#engagement">Nos engagements</a></li>
+							<li><a href="company.php#testimony">Témoignages</a></li>
+							<li><a href="company.php#quotation">Demander un devis</a></li>
+							<li><a href="company.php#news">News</a></li>
+						</ul>
+					</li>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-6">
+				<h3>Nous contacter</h3>
+				<ul class="footer-list">
+					<li><h4>Adresse :</h4><ul class="inner-footer-list">
+						<li>Junior Étude Montpellier Agro</li>
+						<li>2 place Pierre Viala</li>
+						<li>34060 Montpellier Cedex 02</li>
+					</ul></li>
+					<li><h4>Téléphone :</h4><ul class="inner-footer-list">
+						<li>06 83 56 00 00...</li>
+						<li>...ou 06 38 40 96 42 (dépôt de devis)</li>
+						<li>06 99 41 14 02 (président)</li>
+					</ul></li>
+					<li><h4>Mail :</h4><ul class="inner-footer-list">
+						<li>jema@supagro.fr</li>
+						<li>melissa.bernard@supagro.fr (président)</li>
+					</ul></li>
+				</ul>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-6">
+				<div class="col-lg-12">
+					<h3>Mentions légales</h3>
+				</div>
+				<div class="col-lg-12">
+					<h3>Réseaux</h3>
+					<ul class="footer-list">
+						<li>FB</li>
+						<li>TW</li>
+						<li>LK</li>
+						<li>ML</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-6">
+				<h3>Nos partenaires</h3>
+			</div>
 		</footer>
 
 	</div>
--- scripts/index.js
+++ scripts/index.js
@@ -22,23 +22,40 @@ jQuery(document).ready(function($){
           $(this).children('a').stop().css({'color':'#777'});
           $(this).children('a').stop().css({'padding-top':'15px'});
     });
+    $('.last-news').hover(
+      function() {
+          $(this).stop().css({'height':'60px'});
+          $(this).children('p').stop().css({'margin-top':'10px'});
+      },
+      function() {
+          $(this).stop().css({'height':'40px'});
+          $(this).children('p').stop().css({'margin-top':'0px'});
+    });
     $('.student-redirection').hover(
       function() {
           $(this).stop().css({'border':'10px solid white'});
-          $(this).children('p').css({'opacity':'1'});
+          $(this).children().children('p').css({'opacity':'1'});
+          $(this).children().children('.big-glyphicon').css({'animation':'float 2s ease-in-out infinite'});
+          $(".right-student").animate({'right':'30px'},200);
       },
       function() {
           $(this).stop().css({'border':'0px solid white'});
-          $(this).children('p').css({'opacity':'0.2'});
+          $(this).children().children('p').css({'opacity':'0.2'});
+          $(this).children().children('span').css({'animation':'none'});
+          $(".right-student").stop().animate({'right':'-50px'},200);
     });
     $('.company-redirection').hover(
       function() {
           $(this).stop().css({'border':'10px solid white'});
-          $(this).children('p').css({'opacity':'1'});
+          $(this).children().children('p').css({'opacity':'1'});
+          $(this).children().children('span').css({'animation':'float 2s ease-in-out infinite'});
+          $(".right-company").animate({'right':'30px'},200);
       },
       function() {
           $(this).stop().css({'border':'0px solid white'});
-          $(this).children('p').css({'opacity':'0.2'});
+          $(this).children().children('p').css({'opacity':'0.2'});
+          $(this).children().children('span').css({'animation':'none'});
+          $(".right-company").stop().animate({'right':'-50px'},200);
     });
   });
   $(function () {
@@ -50,6 +67,27 @@ jQuery(document).ready(function($){
       }, 800, function(){
         window.location.hash = hash;
       });
+    });
+  });
+  $(function () {
+    if ($(this).scrollTop() > 400){
+      $(".top-anchor").show();
+    }
+    else{
+      $(".top-anchor").hide();
+    }
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 400) {
+        $(".top-anchor").show(10,function(){
+          $(".top-anchor").stop().css({'-webkit-transform':'translateX(0px)'});
+        });
+      }
+      else {
+        $(".top-anchor").stop().css({'-webkit-transform':'translateX(100px)'});
+        if($('.top-anchor').is(':visible')){
+          ('.top-anchor').hide();
+        }
+      }
     });
   });
 });
